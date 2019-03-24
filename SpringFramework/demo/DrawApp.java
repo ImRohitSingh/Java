de33dddc2437ac48d4e68367a3af754539362d16
Rohit Singh <c.ronnie.r@gmail.com>
2019-03-24 13:38:24 +0530

    Main

---
+++ SpringFramework/demo/DrawApp.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package draw;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * @author RS067558
+ *
+ */
+public class DrawApp 
+{
+	public static void main(String[] args) 
+	{
+		/* Note: When using FileSystemXmlApplicationContext keep the .xml within the SpringDemo folder: 
+			ApplicationContext  context =  new FileSystemXmlApplicationContext("Spring.xml"); 
+		*/
+		
+		/* When using ClassPathXmlApplicationContext put the .xml file inside the src folder:*/
+		ApplicationContext  context =  new ClassPathXmlApplicationContext("Spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		triangle.draw();
+	}
+
+}
