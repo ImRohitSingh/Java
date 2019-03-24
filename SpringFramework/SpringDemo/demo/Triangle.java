5f0b8f0f9267e6a1715e58f480ec3e4966c8c169
Rohit Singh <c.ronnie.r@gmail.com>
2019-03-24 13:40:27 +0530

    A Class

---
+++ SpringFramework/SpringDemo/demo/Triangle.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package draw;
+
+/**
+ * @author RS067558
+ *
+ */
+public class Triangle 
+{
+	private String type;
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void draw()
+	{
+		System.out.println(getType()+" Triangle drawn successfully.");
+	}
+}
