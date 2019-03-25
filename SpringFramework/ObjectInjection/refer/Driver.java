988c1123ae631b49288864ba211b7637f55e7304
Rohit Singh <c.ronnie.r@gmail.com>
2019-03-25 14:46:09 +0530

    Main Class

---
+++ SpringFramework/ObjectInjection/refer/Driver.java
@@ -0,0 +1,22 @@
+package refer;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Driver 
+{
+	public static void main(String[] args) 
+	{
+		/* Note: When using FileSystemXmlApplicationContext keep the .xml within the SpringDemo folder: 
+		ApplicationContext  context =  new FileSystemXmlApplicationContext("Spring.xml"); 
+		*/
+		
+		/* When using ClassPathXmlApplicationContext put the .xml file inside the src folder:*/
+		ApplicationContext  context =  new ClassPathXmlApplicationContext("Spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		triangle.draw();
+
+	}
+
+}
