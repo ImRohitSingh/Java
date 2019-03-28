80e57b012dc3823f67b5a5a23a5d5101156bb707
Rohit Singh <c.ronnie.r@gmail.com>
2019-03-28 10:20:36 +0530

    Class containing a list of Points

---
+++ SpringFramework/CollectionInitialization/refer/Triangle.java
@@ -0,0 +1,23 @@
+package refer;
+
+import java.util.List;
+
+public class Triangle 
+{
+	List<Point> points;
+
+	public List<Point> getPoints() {
+		return points;
+	}
+
+	public void setPoints(List<Point> points) {
+		this.points = points;
+	}
+	
+	public void draw() {
+		
+		for(Point point: points)
+			System.out.println("Point: ("+point.getX()+","+point.getY()+")");
+		
+	}
+}
