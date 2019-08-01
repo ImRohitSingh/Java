b8d21344cae557fd4e330354aeaac22cb3e93ba3
Rohit Singh <c.ronnie.r@gmail.com>
2019-08-01 10:24:38 +0530

    MathSumWithKey

---
+++ misc/src/com/rohit/searches/MatchSumWithKey.java
@@ -0,0 +1,41 @@
+package com.rohit.searches;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatchSumWithKey {
+	
+	private static List<Integer> indexes = new ArrayList();
+	
+	public static void main(String[] args) {
+		
+		int array[] = {1,2,3,4,5}; // assuming array is sorted in ascending order
+		int key = 15;
+		
+		indexes = new MatchSumWithKey().findIndexes(0,array.length-1,array,key);
+
+		if(indexes.isEmpty()) {
+			System.out.println("No matched indexes...");
+		} else {
+			System.out.println(indexes.toString());
+		}
+		
+	}
+	
+	private List<Integer> findIndexes(int low,int high,int array[],int key) {
+		
+		while(low<high) {
+			if(array[low]+array[high] == key) {
+				indexes.add(low);
+				indexes.add(high);
+				break;
+			} else if(array[low]+array[high] < key) {
+				++low;
+			} else {
+				--high;
+			}
+		}
+		return indexes;
+	}
+
+}
