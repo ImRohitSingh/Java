20c3a1537e6717e24c82193bce95f54708a6174d
Rohit Singh <c.ronnie.r@gmail.com>
2019-08-05 11:13:17 +0530

    Represents a Node

---
+++ misc/src/com/rohit/singlylinkedlist/SinglyLinkedListNode.java
@@ -0,0 +1,25 @@
+package com.rohit.singlylinkedlist;
+
+public class SinglyLinkedListNode<T> {
+
+	private T info;
+	private SinglyLinkedListNode<T> next;
+	
+	public T getInfo() {
+		return info;
+	}
+	
+	public void setInfo(T info) {
+		this.info = info;
+	}
+
+	public SinglyLinkedListNode<T> getNext() {
+		return next;
+	}
+
+	public void setNext(SinglyLinkedListNode<T> next) {
+		this.next = next;
+	}
+	
+	
+}
