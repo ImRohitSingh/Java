1c2293ebac64380605c8fb1910e826162fd6c76b
Rohit Singh <c.ronnie.r@gmail.com>
2019-08-14 18:07:42 +0530

    An item of a stack

---
+++ misc/src/com/rohit/stack/Node.java
@@ -0,0 +1,25 @@
+package com.rohit.stack;
+
+public class Node<T> {
+
+	private T info;
+	
+	private Node<T> next;
+
+	public T getInfo() {
+		return info;
+	}
+
+	public void setInfo(T info) {
+		this.info = info;
+	}
+
+	public Node<T> getNext() {
+		return next;
+	}
+
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+	
+}
